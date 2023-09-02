b3d43e11b7d180a02fbbeae5d68a5f3bdbc0c054
Michał Baran <michal.baran.dev@gmail.com>
2023-09-02 21:18:02 +0200

    Simple editor srcipts

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-const textarea = document.querySelector('.textarea--js')
-const loadButton = document.querySelector('.load--js')
-const saveButton = document.querySelector('.save--js')
+const textarea = document.querySelector('.textarea--js');
+console.log(textarea)
+
+const loadButton = document.querySelector('.load--js');
+console.log(loadButton)
+
+const saveButton = document.querySelector('.save--js');
+console.log(saveButton)
+
+saveButton.addEventListener('click', () => {localStorage.setItem('content', textarea.value)})
+
+loadButton.addEventListener('click', () => {textarea.value = localStorage.getItem('content')})
