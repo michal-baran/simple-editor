f7bac0ead2904db875349ab50a8d0c98b2d75346
Michal Baran <michal.baran.dev@gmail.com>
2023-09-30 17:30:56 +0200

    Simply editor design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
